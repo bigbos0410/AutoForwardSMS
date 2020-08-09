da745f6e249b67de8647cd2b2e11dc977b4f22ef
Trung Kien Tran <kientt13.7@gmail.com>
2020-08-10 01:29:33 +0700

    - enable send to phone

--- app/src/main/java/com/ttk/lab/autoforwardsms/provider/SmsBroadcastReceiver.java
+++ app/src/main/java/com/ttk/lab/autoforwardsms/provider/SmsBroadcastReceiver.java
@@ -13,21 +13,24 @@ import static com.ttk.lab.autoforwardsms.Constants.TAG;
 
 public class SmsBroadcastReceiver extends BroadcastReceiver {
     private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+//    private static final String BOOT_COMPLETED = "android.intent.action.BOOT_COMPLETED";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction() != null && intent.getAction().equals(SMS_RECEIVED)) {
-            Bundle bundle = intent.getExtras();
-            if (bundle != null) {
-                Object[] pdus = (Object[])bundle.get("pdus");
-                if (pdus != null && pdus.length > 0) {
-                    final SmsMessage[] messages = new SmsMessage[pdus.length];
-                    for (int i = 0; i < pdus.length; i++) {
-                        messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], bundle.getString("format"));
+        if (intent.getAction() != null) {
+            if(intent.getAction().equals(SMS_RECEIVED)) {
+                Bundle bundle = intent.getExtras();
+                if (bundle != null) {
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    if (pdus != null && pdus.length > 0) {
+                        final SmsMessage[] messages = new SmsMessage[pdus.length];
+                        for (int i = 0; i < pdus.length; i++) {
+                            messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], bundle.getString("format"));
+                        }
+                        Log.d(TAG, "Message recieved: " + messages[0].getMessageBody());
+                        String msg_content = "<FROM> " + messages[0].getOriginatingAddress() + "\n" + messages[0].getMessageBody();
+                        new ForwardSmsAsyncTask(context).execute(msg_content);
                     }
-                    Log.i(TAG, "Message recieved: " + messages[0].getMessageBody());
-                    String msg_content = "<FROM> " + messages[0].getOriginatingAddress() + "\n" + messages[0].getMessageBody();
-                    new ForwardSmsAsyncTask(context).execute(msg_content);
                 }
             }
         }
